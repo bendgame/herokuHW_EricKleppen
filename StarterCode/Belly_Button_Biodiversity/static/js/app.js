f588d6ad724a778ce09c962a75832d433f28b6a6
bendgame <erickleppen01@gmail.com>
2019-06-11 20:37:56 -0500

    some first lines

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,32 +1,48 @@
-function buildMetadata(sample) {
-  console.log("Build metadata"); 
 
-  // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+// function buildMetadata(sample) {
+//   console.log("Build metadata"); 
 
-    // Use `.html("") to clear any existing metadata
+//   // @TODO: Complete the following function that builds the metadata panel
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+//   // Use `d3.json` to fetch the metadata for a sample
+//     // Use d3 to select the panel with id of `#sample-metadata`
+//     var url = `/metadata/${sample}`;
+    
+//     d3.json(url).then(function(response) {
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-}
+//       sample_meta = d3.select("#sample-metadata")
 
-function buildCharts(sample) {
-  console.log("Build new chart");
+//     // Use `.html("") to clear any existing metadata
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+//       sample_meta = d3.select("#sample-metadata")
+//       sample_meta.html("")
+//     // Use `Object.entries` to add each key and value pair to the panel
+//       Object.entries(response).forEach(([key, value]) => {
+//       var cell = row.append("p");
+//       cell.text(value);
+//       });
+//     });
 
-    // @TODO: Build a Bubble Chart using the sample data
+    
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+//     // BONUS: Build the Gauge Chart
+//     // buildGauge(data.WFREQ);
+// };
+
+// function buildCharts(sample) {
+//   console.log("Build new chart");
+
+//   // @TODO: Use `d3.json` to fetch the sample data for the plots
+
+//     // @TODO: Build a Bubble Chart using the sample data
+
+//     // @TODO: Build a Pie Chart
+//     // HINT: You will need to use slice() to grab the top 10 sample_values,
+//     // otu_ids, and labels (10 each).
+// };
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -46,13 +62,13 @@ function init() {
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
-}
+};
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
-}
+};
 
 // Initialize the dashboard
 init();
